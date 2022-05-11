c0dbae2156c40f33dd9648c109bc1e8b66f6729b
JaimeDev <jaimegaudencio@gmail.com>
2022-05-11 16:26:38 -0300

    anotacoes das entidades parte 1

--- src/main/java/me/dio/academia/digital/entity/AvaliacaoFisica.java
+++ src/main/java/me/dio/academia/digital/entity/AvaliacaoFisica.java
@@ -1,17 +1,33 @@
 package me.dio.academia.digital.entity;
 
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+import javax.persistence.*;
 import java.time.LocalDateTime;
 
+@Data
+@NoArgsConstructor
+@AllArgsConstructor
+@Entity
+@Table(name = "tb_avaliacoes")
 public class AvaliacaoFisica {
 
-  private Long id;
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
 
-  private Aluno aluno;
+    @ManyToOne(cascade = CascadeType.ALL)
+    @JoinColumn(name = "aluno_id")
+    private Aluno aluno;
 
-  private LocalDateTime dataDaAvaliacao = LocalDateTime.now();
+    private LocalDateTime dataDaAvaliacao = LocalDateTime.now();
 
-  private double peso;
+    @Column(name = "peso_atual")
+    private double peso;
 
-  private double altura;
+    @Column(name = "altura_atual")
+    private double altura;
 
 }
