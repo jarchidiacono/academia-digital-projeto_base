5c69c73f0e8a0aae4cfa311066b7d339ab1b161c
JaimeDev <jaimegaudencio@gmail.com>
2022-05-12 17:20:52 -0300

    projeto finalizado

--- src/main/java/me/dio/academia/digital/entity/form/AvaliacaoFisicaForm.java
+++ src/main/java/me/dio/academia/digital/entity/form/AvaliacaoFisicaForm.java
@@ -5,14 +5,24 @@ import lombok.Data;
 import lombok.NoArgsConstructor;
 import me.dio.academia.digital.entity.Aluno;
 
+import javax.validation.constraints.DecimalMin;
+import javax.validation.constraints.NotNull;
+import javax.validation.constraints.Positive;
+
 @Data
 @NoArgsConstructor
 @AllArgsConstructor
 public class AvaliacaoFisicaForm {
 
+  @Positive(message = "O Id do aluno precisa ser positivo")
   private Long alunoId;
 
+  @NotNull(message = "Preencha o campo corretamente.")
+  @Positive(message = "O Id do aluno precisa ser positivo")
   private double peso;
 
+  @NotNull(message = "Preencha o campo corretamente.")
+  @Positive(message = "O Id do aluno precisa ser positivo")
+  @DecimalMin(value = "150", message = "'${validateValue}' precisa ser no mínimo {value}.")
   private double altura;
 }
